51bd31d9e09eefed61e19d33e6b68d6f2b8f9255
Allan Tatter <allan.tatter@gmail.com>
2017-02-07 22:27:42 +0200

    Add WordPress install command to artisan

--- src/WordPress/WordPressCliServiceProvider.php
+++ src/WordPress/WordPressCliServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace FilcPress\WordPress;
 
+use DB;
+use Illuminate\Database\QueryException;
 use Illuminate\Support\ServiceProvider;
 
 class WordPressCliServiceProvider extends ServiceProvider
@@ -29,6 +31,17 @@ class WordPressCliServiceProvider extends ServiceProvider
             return;
         }
 
+        if (! $this->isWordPressInstalled()) {
+            $this->setWordPressStatusToInstalling();
+        }
+
+        // Set common headers, to prevent warnings from plugins
+        $_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.0';
+        $_SERVER['SERVER_NAME'] = env('APP_DOMAIN', 'filcpress.dev');
+        $_SERVER['HTTP_USER_AGENT'] = '';
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+
         // Load the WordPress library.
         require(config('wordpress.path').'/wp-load.php');
     }
@@ -36,5 +49,21 @@ class WordPressCliServiceProvider extends ServiceProvider
     private function isCli()
     {
         return php_sapi_name() === 'cli';
+    }
+
+    private function isWordPressInstalled()
+    {
+        try {
+            $siteUrlRow = DB::table('wp_options')->where('option_name', 'siteurl')->first();
+        } catch (QueryException $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function setWordPressStatusToInstalling()
+    {
+        define('WP_INSTALLING', true);
     }
 }
