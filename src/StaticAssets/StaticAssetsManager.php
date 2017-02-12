cd29c988366cef8b2501e928c9763cf8305dd598
Allan Tatter <allan.tatter@gmail.com>
2017-02-12 22:16:17 +0200

    Add static assets

---
+++ src/StaticAssets/StaticAssetsManager.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace FilcPress\StaticAssets;
+
+use Illuminate\Container\Container;
+
+class StaticAssetsManager
+{
+    public function registerStylesheet($src, $name = 'theme')
+    {
+        wp_register_style($name, url($src));
+        wp_enqueue_style($name);
+    }
+    public function registerScript($src, $name = 'theme')
+    {
+        wp_register_script($name, url($src), null, null, true);
+        wp_enqueue_script($name);
+    }
+}
