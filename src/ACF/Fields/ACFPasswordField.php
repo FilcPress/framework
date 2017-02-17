52b03a59b53d1ecdb24459bc502e99ac074b955d
Allan Tatter <allan.tatter@gmail.com>
2017-02-17 20:07:10 +0200

    Remove unused imports

--- src/ACF/Fields/ACFPasswordField.php
+++ src/ACF/Fields/ACFPasswordField.php
@@ -6,8 +6,6 @@ use FilcPress\ACF\ACFField;
 use FilcPress\ACF\Options\AppendOption;
 use FilcPress\ACF\Options\PrependOption;
 use FilcPress\ACF\Options\PlaceholderOption;
-use FilcPress\ACF\Options\DefaultValueOption;
-use FilcPress\ACF\Options\CharacterLimitOption;
 
 class ACFPasswordField extends ACFField
 {
