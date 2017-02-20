81b8b775ff455464b54f86b07ec3dbcf81ea04ea
Allan Tatter <allan.tatter@gmail.com>
2017-02-20 20:23:25 +0200

    Register scripts only in front-end

--- src/StaticAssets/StaticAssetsManager.php
+++ src/StaticAssets/StaticAssetsManager.php
@@ -8,12 +8,16 @@ class StaticAssetsManager
 {
     public function registerStylesheet($src, $name = 'theme')
     {
-        wp_register_style($name, url($src));
-        wp_enqueue_style($name);
+        add_action('wp_enqueue_scripts', function () use ($src, $name) {
+            wp_register_style($name, url($src));
+            wp_enqueue_style($name);
+        });
     }
     public function registerScript($src, $name = 'theme')
     {
-        wp_register_script($name, url($src), null, null, true);
-        wp_enqueue_script($name);
+        add_action('wp_enqueue_scripts', function () use ($src, $name) {
+            wp_register_script($name, url($src), null, null, true);
+            wp_enqueue_script($name);
+        });
     }
 }
